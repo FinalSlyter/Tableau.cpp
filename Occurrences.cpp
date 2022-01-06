9438e734709c2d6d6fdccc00678a509ed46814d1
Florent <91935659+FinalSlyter@users.noreply.github.com>
2022-01-06 18:44:40 +0100

    Create Occurrences.cpp

---
+++ Occurrences.cpp
@@ -0,0 +1,21 @@
+                                  // Compter les occurrences d’une valeur
+#include <vector>
+
+int main()
+{
+    std::vector<char> const lettres { 'a', 'b', 'b', 'c', 'b', 'd', 'd', 'a' };
+    char const lettre_a_compter { 'b' };
+    int total { 0 };
+
+    for (auto const lettre : lettres)
+    {
+        if (lettre == lettre_a_compter)
+        {
+            ++total;
+        }
+    }
+
+    std::cout << "La lettre '" << lettre_a_compter << "' est presente " << total << " fois." << std::endl;
+
+    return 0;
+}
